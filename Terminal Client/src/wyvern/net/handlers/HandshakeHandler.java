99b9759e51a07151bf4473d7e44d66c6a255d878
Owen2k6 <fulptdm@gmail.com>
2023-05-07 21:47:35 +0100

    hm

--- Terminal Client/src/wyvern/net/handlers/HandshakeHandler.java
+++ Terminal Client/src/wyvern/net/handlers/HandshakeHandler.java
@@ -1,12 +1,13 @@
 package wyvern.net.handlers;
 
+import wyvern.Client;
 import wyvern.net.AbstractPacket;
 import wyvern.net.PacketHandler;
 import wyvern.net.packets.Packet0Handshake;
 
 public class HandshakeHandler implements PacketHandler
 {
-    public void handlePacket(AbstractPacket packet)
+    public void handlePacket(Client a, AbstractPacket packet)
     {
         try
         {
